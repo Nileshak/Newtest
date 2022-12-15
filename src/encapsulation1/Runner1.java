3156bca6c9d612ce5d3b3c90e81056ee030ce7ce
niles <niles@LAPTOP-SKAISQO4>
2022-12-15 22:27:55 +0530

    hi nilesh

---
+++ src/encapsulation1/Runner1.java
@@ -0,0 +1,20 @@
+package encapsulation1;
+
+public class Runner1 extends encapsultion_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-
+		
+    encapsultion_demo en=new encapsultion_demo();
+    en.get();
+    en.get1();
+    
+    System.out.println("check balance of remaining:"+en.get()
+    );
+    
+    
+    
+    
+	}
+
+}
---
+++ src/encapsulation1/encapsultion_demo.java
@@ -0,0 +1,31 @@
+package encapsulation1;
+
+public class encapsultion_demo {
+
+	private String name="Nilesh";
+	private int Bank_bal=1000;
+	private String check_bal;
+	
+	public void set(String n,int b) {
+		n=this.name;
+		b=this.Bank_bal;
+		
+	}
+	public String get() {
+		check_bal=(name+" "+Bank_bal);
+		return check_bal;
+		
+		
+	}
+	public void set1(String n1,int b1) {
+		n1=this.name;
+		b1=this.Bank_bal;
+			
+}
+	public String get1() {
+		check_bal=(name+" "+Bank_bal);
+		return check_bal;
+		
+}}
+
+
